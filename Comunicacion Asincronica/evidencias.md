1fb6b628981b9d1814b5ef218353a87d45671f90
lauracoicue <coicue.laura@correounivalle.edu.co>
2025-06-18 12:00:08 -0500

    primero codelabs

---
+++ Comunicacion Asincronica/evidencias.md
@@ -0,0 +1,11 @@
+# Evidencias de ejecución de los Microservicios de comunicación Asincronica
+
+
+---
+## Comunicación Asincronica - Microservicio de Pedido
+![](img/evidencia_pedido_Asincronica.jpg)
+
+---
+## Comunicación Asincronica - Microservicio de Producto
+![](img/evidencias_Producto.jpg)
+
---
+++ Comunicacion Asincronica/producto-service/src/main/java/com/productoservice/productoservice/ProductoController.java
@@ -0,0 +1,21 @@
+package com.productoservice.productoservice;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+    private final ProductorService productorService;
+
+    public ProductoController(ProductorService productorService) {
+        this.productorService = productorService;
+    }
+
+    @PostMapping("/enviar")
+    public String enviarProductos(@RequestBody List<ProductoDTO> productos) {
+        productorService.enviarListaProductos(productos);
+        return "Productos enviados con éxito al pedido-service";
+    }
+}
